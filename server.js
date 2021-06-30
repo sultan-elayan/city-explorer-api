b813560aa3f14817e0712c9dcbc09bf6f6f68cf9
sultan-elayan <sultan.m.elayan@gmail.com>
2021-06-30 14:54:31 +0300

    adding the backend

--- server.js
+++ server.js
@@ -1,50 +1,88 @@
 const express = require('express') // require the express package
-let weatherData = require('./data/weather.json')
 const app = express() // initialize your express app instance
 const cors = require('cors');
+const axios = require('axios'); // require the package
+let weatherData = require('./data/weather.json')
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 app.use(cors()) // after you initialize your express app instance
 
 
-app.get('/weather', (req, res) => {
-    let lat = req.query.lat
-    let lon = req.query.lon
-    let searchQuery = req.query.searchQuery
+let WEATHER_API_KEY= process.env.WEATHER_API_KEY
+let MOVIE_API_KEY=process.env.MOVIE_API_KEY
 
 
 
-    let findData = () => {
-        let city = weatherData.find((city, index) => {
-            return city.city_name.toLocaleLowerCase() === searchQuery.toLocaleLowerCase()
-        })
+app.get('/weather', (req, res)=>{
+    let weatherData;
+    let lat=req.query.lat;
+    let lon=req.query.lon;
+    
+let url=`https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
+let weatherResponse=axios.get(url).then(response => {
+    weatherData=response.data;
+  let forecast=weatherData.data.map(item=>{
+    return new Forecast(item)
+  })
 
-        return city.data.map(item => {
+  res.json(forecast)}).catch(err=>{res.status(500).send(`error in getting data ==> ${err}`)})});
 
-       return new ForeCast(item)
-        }
-        )
-    }
 
-    res.json(findData());
-}
-);
+
+app.get('/movies', (req, res)=>{
+    let city_name=req.query.city
+   let urlMovie=`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${city_name}`
+ 
+   let movieResponse=axios.get(urlMovie).then(response => {
+    
+     let movie=response.data.results.map(item=>{
+       return new Movie(item)
+     })
+     res.json(movie)
+   }).catch(err=>{
+     res.status(500).send(`error in getting data ==> ${err}`)
+ })
+ });
+
+
 
 class ForeCast {
     constructor(weather) {
 
         this.date = weather.valid_date
-        this.description = weather.weather.description
+        this.description = weather.weather.description}};
 
-    }
-}
 
+class Movie{
+    constructor(data){
+      this.average_votes = data.vote_average;
+  
+      this.total_votes = data.vote_count
+  
+      this.image_url = data.backdrop_path
+  
+      this.popularity = data.popularity
+  
+      this.released_on = data.release_date }};
 
 // app.listen(process.PORT) // kick start the express server to work
-app.listen(PORT, () => {
-    console.log(`starting  ${PORT}`)
-})
+app.listen(PORT, () => {console.log(`starting  ${PORT}`)})
 
+
+// ======================================================================================
+
+
+/// here to getting data from json file (old work)
+// app.get('/weather', (req, res) => {
+//     let lat = req.query.lat
+//     let lon = req.query.lon
+//     let searchQuery = req.query.searchQuery
+//     let findData = () => {
+//         let city = weatherData.find((city, index) => {
+//             return city.city_name.toLocaleLowerCase() === searchQuery.toLocaleLowerCase()})
+//         return city.data.map(item => {
+//        return new ForeCast(item)})}
+// res.json(findData());});
 
 // ======================================================================================
 
